51213b4585dc7c3325c0cf04ffc45b8b41a121c3
wynne baek <wynne.baek@gmail.com>
2022-05-19 16:06:42 +0900

    complete model & add data

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from movies.models import Movie
 
 class User(AbstractUser):
-    pass
+    following = models.ManyToManyField('self', symmetrical=False, related_name='follower')
+    watched_list = models.ManyToManyField(Movie, related_name='watched_list')
--- community/models.py
+++ community/models.py
@@ -1,3 +1,21 @@
+import imp
 from django.db import models
+from django.conf import settings
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+class Review(models.Model):
+    title = models.CharField(max_length=100)
+    movie_title = models.CharField(max_length=50)
+    rate = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    review = models.ForeignKey(Review, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
