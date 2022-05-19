ff83a2031e3c04c95495486905286a606edc18a6
wynne baek <wynne.baek@gmail.com>
2022-05-19 17:52:30 +0900

    fix typo

--- community/forms.py
+++ community/forms.py
@@ -6,11 +6,11 @@ class ReviewForm(forms.ModelForm):
     
     class Meta:
         model = Review
-        fields = ['title', 'movie_title', 'rank', 'content']
+        fields = ('title', 'movie_title', 'rate', 'content')
 
 
 class CommentForm(forms.ModelForm):
 
     class Meta:
         model = Comment
-        exclude = ['review', 'user']
+        exclude = ('review', 'user')
--- community/models.py
+++ community/models.py
@@ -1,4 +1,3 @@
-import imp
 from django.db import models
 from django.conf import settings
 from django.core.validators import MinValueValidator, MaxValueValidator
