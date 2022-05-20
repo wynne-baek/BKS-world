ad3016dcaf6fc257c31d0c43f43bdb455f0167fd
wynne baek <wynne.baek@gmail.com>
2022-05-20 11:15:56 +0900

    movies view complete

--- movies/urls.py
+++ movies/urls.py
@@ -5,7 +5,7 @@ app_name = 'movies'
 
 urlpatterns = [
     path('', views.movie_home, name='movie_home'),
-    path('list/', views.movie_list, name='movie_list'),
+    path('list/', views.search_movie, name='search_movie'),
     path('<int:movie_pk>/', views.movie_detail, name='movie_detail'),
 
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,13 +1,29 @@
-from django.shortcuts import render, redirect, get_object_or_404
-
+from django.shortcuts import get_list_or_404, render, redirect, get_object_or_404
+from .models import Movie
 
 def movie_home(request):
     return render(request, 'movies/moviehome.html')
 
-def movie_list(request):
-    return render(request, 'movies/movielist.html')
+def search_movie(request):
+    # 검색 버튼을 눌렀을 때 실행,
+    search = request.GET.get('search', '')
+    if search:
+        movies = Movie.objects.filter(country=f'{search}')
+    # 1. 로그인 여부 확인
+    # 2. 로그인 되어 있을 경우, 검색어 쿼리에 넣어서 해당 국가 정보가 들어있는 영화만 보여줌
+    if request.user.is_authenticated:
+        context = {
+            'movies': movies,
+        }
+        return render(request, 'movies/movielist.html', context)
+    # 1-2. 로그인 되어있지 않으면, 로그인 페이지로
+    return redirect('accounts:login')
 
-def movie_detail(request):
-    return render(request, 'movies/moviedetail.html')
+def movie_detail(request, movie_pk):
+    movie = get_object_or_404(Movie, pk=movie_pk)
+    context = {
+        'movie': movie,
+    }
+    return render(request, 'movies/moviedetail.html', context)
 
 
