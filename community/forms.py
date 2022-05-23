023d12d0b2199ffac4420f5ddb458702d788585d
wynne baek <wynne.baek@gmail.com>
2022-05-23 18:09:16 +0900

    edit community

--- community/forms.py
+++ community/forms.py
@@ -1,12 +1,19 @@
 from django import forms
 from .models import Review, Comment
 
+RATE_CHOICE = [tuple([x, x]) for x in range(1, 6)]
 
 class ReviewForm(forms.ModelForm):
     
     class Meta:
         model = Review
         fields = ('title', 'movie_title', 'rate', 'content')
+        widgets = {
+            'title': forms.Textarea(attrs={'class': 'form-control my-2', 'rows': 1, 'style': 'width: 300px;'}),
+            'movie_title': forms.Textarea(attrs={'class': 'form-control my-2', 'rows': 1, 'style': 'width: 300px;'}),
+            'rate': forms.Select(choices=RATE_CHOICE),
+            'content': forms.Textarea(attrs={'class': 'form-control my-2', 'style': 'width: 680px;', 'rows': 10})
+        }
 
 
 class CommentForm(forms.ModelForm):
