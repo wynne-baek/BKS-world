849f5094a6909713cac53dcc22b00a5675bf0ea7
wynne baek <wynne.baek@gmail.com>
2022-05-24 16:47:31 +0900

    account complete

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.contrib.auth import get_user_model
 
 
@@ -7,3 +8,9 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
         fields = UserCreationForm.Meta.fields
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = get_user_model()
+        fields = ('first_name', 'last_name',)
